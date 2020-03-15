0481b6205a23494420b89922dd8bf3586716d664
chrischase <chr15t0pher.cha53@gmail.com>
2020-03-15 08:20:17 +0000

    basic server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Listee');
+});
+
+app.listen(3000, () => {
+    console.log('Listee is listening on port 3000');
+});
