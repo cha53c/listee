bf65b1f71f4abcef502e38f27ca8e6c17dfe6da8
chrischase <chr15t0pher.cha53@gmail.com>
2020-03-15 12:31:04 +0000

    added ejs engine

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const debug = require('debug')('app');
 const chalk = require('chalk');
 const path = require('path');
+const ejs = require('ejs');
 
 const lists = require(path.join(__dirname, 'routes', 'listRoutes'));
 const app = express();
 app.use('/', lists);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     debug(`Listee is listening to you on port ${chalk.green('3000')}`);
--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+// const ejs = require('ejs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome to Listee');
+    res.render('home', {title: 'Listee home page'});
 });
 
 module.exports = router;
