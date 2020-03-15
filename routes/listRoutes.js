035de275db662835ac686cb4c8de5b5c95abe1cf
chrischase <chr15t0pher.cha53@gmail.com>
2020-03-15 18:02:35 +0000

    added paramterised routes

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('lists', {title: 'your lists', heading: 'Listee keeps all your lists are here'});
+router.get('/:userId', (req, res) => {
+    if(req.params.userId == 1) { //simulate logged in
+        res.render('lists', {title: 'your lists', heading: 'Listee keeps all your lists are here'});
+    } else {
+        res.redirect('/'); // if not logged in redirect back to home page
+    }
 });
+
+router.get('/create', (req, res) => {
+    res.render('createList', {title: 'create new list', heading: 'Create your new list'});
+});
+
+router.get('/:userId/:listId', (req, res) => {
+    res.render('show', { listId: req.params.listId});
+})
 
 module.exports = router;
 
