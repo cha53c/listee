97892e4c66c8fab37f8210658a134e844999d192
chrischase <chr15t0pher.cha53@gmail.com>
2020-03-17 07:57:35 +0000

    refactored auth page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,16 +8,14 @@ router.post('/signUp', (req, res) => {
     debug('sign up user');
 });
 
-router.post('/signIn', (req, res, next) => {
+router.post('/login', (req, res, next) => {
     debug('log in user');
     res.redirect('/lists/1')
 });
 
 router.get('/', (req, res) => {
     debug('auth');
-    res.render('signin');
+    res.render('auth');
 });
-
-
 
 module.exports = router;
