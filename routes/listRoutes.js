09a4e13cdfebff46f40a99fb454dffc8b700acf5
chrischase <chr15t0pher.cha53@gmail.com>
2020-03-21 18:01:16 +0000

    create list in page

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -4,8 +4,8 @@ const chalk = require('chalk');
 const router = express.Router();
 
 router.get('/:userId', (req, res) => {
-    const userId = req.params.userId
-    debug(`lists for user ${chalk.magenta(userId)}` )
+    const userId = req.params.userId;
+    debug(`lists for user ${chalk.magenta(userId)}`);
     if(req.params.userId == 1) { //simulate logged in
         res.render('lists', {title: 'your lists', heading: 'Listee keeps all your lists are here'});
     } else {
@@ -13,13 +13,14 @@ router.get('/:userId', (req, res) => {
     }
 });
 
-router.get('/create', (req, res) => {
+router.get('/create/:userId/', (req, res) => {
     debug('create');
     res.render('createList', {title: 'create new list', heading: 'Create your new list'});
 });
 
-router.get('/:userId/:listId', (req, res) => {
-    debug('show');
+router.get('show/:userId/:listId', (req, res) => {
+    const listId = req.params.listId;
+    debug(`show list: ${chalk.magenta(listId)}`);
     res.render('show', { listId: req.params.listId});
 })
 
