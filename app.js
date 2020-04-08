d46cb006170edf968b5a9943e388be34f5f8f898
chrischase <chr15t0pher.cha53@gmail.com>
2020-04-08 22:08:30 +0100

    moved list views to own directory

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/', home);
 app.use('/lists', lists);
 app.use('/auth', auth);
 
-app.set('views', './views');
+app.set('views', ['./views', 'views/list'] );
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
