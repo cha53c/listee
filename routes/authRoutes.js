756d5c5f6d2d3daf86edd4dc17470df78094471d
chrischase <chr15t0pher.cha53@gmail.com>
2020-04-09 07:36:01 +0100

    move login and sign up to separate pages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,32 +5,41 @@ const chalk = require('chalk');
 const userStore = require('../model/userStore')
 
 const router = express.Router();
+router.get('/signUp', (req, res) => {
+    debug('sign up route');
+    res.render('sighup');
+});
 
-    router.post('/signUp', (req, res) => {
-        debug('sign up user');
-        userStore.addUser(req.body.username);
-        debug(req.body);
-        res.render('auth');
-        // res.json(req.body);
-    });
+router.post('/signUp', (req, res) => {
+    debug('sign up user');
+    userStore.addUser(req.body.username);
+    debug(req.body);
+    res.render('auth');
+    // res.json(req.body);
+});
 
-    router.get('/', (req, res) => {
-        debug('auth');
-        res.render('auth');
-    });
+router.get('/', (req, res) => {
+    debug('auth');
+    res.render('auth');
+});
 
-    router.post('/login', (req, res, next) => {
-        let user = req.body.username;
-        debug(`attempting to log in user ${user}`);
-        debug(req.body);
-        if(userStore.isUser(user)) {
+router.get('/login', (req, res) => {
+    debug('login route');
+    res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+    let user = req.body.username;
+    debug(`attempting to log in user ${user}`);
+    debug(req.body);
+    if (userStore.isUser(user)) {
         // if(true) {
-            debug('redirecting to list/1')
-            res.redirect('/lists/1')
-        } else {
-            debug('user not found')
-            res.redirect('/');
-        }
-    });
+        debug('redirecting to list/1')
+        res.redirect('/lists/1')
+    } else {
+        debug('user not found')
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
