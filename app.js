cc526e6ee53cd18c795364a24d6e463cb378275f
chrischase <chr15t0pher.cha53@gmail.com>
2020-04-21 08:07:33 +0100

    moved css in lists home to external file

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/static', express.static('public/css'));
 // set up the routes
 app.use('/', home);
 app.use('/lists', lists);
