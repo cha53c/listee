f5b4d9c4dc9f3b90f6c72d8a0f0b2b2ce1baacbb
chrischase <chr15t0pher.cha53@gmail.com>
2020-05-18 18:17:54 +0100

    pulling in audio urls from OED

--- controllers/beeController.js
+++ controllers/beeController.js
@@ -1,16 +1,25 @@
 const debug = require('debug')('app:beeController');
-const { red } = require('chalk');
+const {red} = require('chalk');
 const path = require('path');
 const audioService = require('../services/OEDService');
 
 function getTest(req, res) {
+    const words = ['egg', 'butter', 'flour', 'sugar']
+    // const words = ['flour'];
     debug('get test');
-    res.render('test', {title: 'spelling test', heading: 'Spelling test'});
+    (async function oed() {
+        // TODO need to handle rejects currently hangs if word not found.
+        const promises = words.map(word => audioService.getPronunciationAudioUrl(word));
+        const urls = await Promise.all(promises);
+        console.log(urls);
+        res.render('test', {title: 'spelling test', heading: 'Spelling test', audioUrls: urls});
+    }());
+
 }
 
-function getAudio(req, res){
-    debug('get audio');
-    res.json(audioService.getPronunciation());
+function getAudio() {
+
 }
 
-module.exports = { getTest, getAudio }
+
+module.exports = {getTest, getAudio}
--- services/OEDService.js
+++ services/OEDService.js
@@ -1,30 +1,27 @@
 const debug = require('debug')('app:OEDService');
 const axios = require('axios');
 
-function getPronunciation() {
+function getPronunciationAudioUrl(word) {
     debug('get entry');
-    const endPoint = 'entries';
-    const sourceLan = 'en-gb';
-    const word = 'badger';
-    axios.get( 'entries/en-gb/badger', {
-        baseURL: process.env.OED_BASE_URL,
-        params: {},
-        headers: {
-            'app_id': process.env.OED_APP_ID,
-            'app_key': process.env.OED_APP_KEY
-        }
-    })
-        .then(function (response) {
-            debug(response);
+    return new Promise((resolve, reject) => {
+        axios.get( 'entries/en-gb/' + word, {
+            baseURL: process.env.OED_BASE_URL,
+            params: {},
+            headers: {
+                'app_id': process.env.OED_APP_ID,
+                'app_key': process.env.OED_APP_KEY
+            }
         })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .finally(function () {
-            // always executed
-        });
-    // TODO return audio from OED
-    return 'audio goes here';
+            .then(function (response) {
+                debug(response);
+                let audioUrl = response.data.results[0].lexicalEntries[0].pronunciations[0].audioFile;
+                debug('audioUrl %s', audioUrl);
+                resolve(audioUrl);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    });
 }
 
-module.exports = {getPronunciation};
+module.exports = {getPronunciationAudioUrl};
