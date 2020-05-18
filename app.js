eb1c1b89bbe310f8791a511f8ed76b60437688ab
chrischase <chr15t0pher.cha53@gmail.com>
2020-05-18 07:49:31 +0100

    add dotenv and .env

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const ejs = require('ejs');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const envLoad = require('dotenv').config();
 
 const home = require(path.join(__dirname, 'routes', 'homeRoutes'));
 const lists = require(path.join(__dirname, 'routes', 'listRoutes'));
@@ -12,9 +13,14 @@ const auth = require(path.join(__dirname, 'routes', 'authRoutes'));
 const bee = require(path.join(__dirname, 'routes', 'beeRoutes'));
 const app = express();
 
+if(envLoad.error){
+    throw envLoad.error;
+}
+
 process.title = "listeeApp";
 
-debug('node environment = %s', process.env.NODE_ENV);
+debug('.env variable %s', envLoad.parsed);
+debug('node environment: %s', process.env.NODE_ENV);
 
 if(process.env.NODE_ENV !== 'test' ){
     app.use(morgan('tiny'));
@@ -35,7 +41,7 @@ app.use('/bee', bee);
 app.set('views', ['./views', 'views/list', 'views/auth', 'views/bee'] );
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
     debug(`Listee is listening to you on port ${chalk.green(PORT)}`);
 });
