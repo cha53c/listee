18a139eaecaecdd5b6d871d7c47bb146dcbadfe2
chrischase <chr15t0pher.cha53@gmail.com>
2020-05-18 08:41:48 +0100

    added axios

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const auth = require(path.join(__dirname, 'routes', 'authRoutes'));
 const bee = require(path.join(__dirname, 'routes', 'beeRoutes'));
 const app = express();
 
-if(envLoad.error){
+if (envLoad.error) {
     throw envLoad.error;
 }
 
@@ -22,7 +22,7 @@ process.title = "listeeApp";
 debug('.env variable %s', envLoad.parsed);
 debug('node environment: %s', process.env.NODE_ENV);
 
-if(process.env.NODE_ENV !== 'test' ){
+if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('tiny'));
 }
 
@@ -38,7 +38,7 @@ app.use('/lists', lists);
 app.use('/auth', auth);
 app.use('/bee', bee);
 
-app.set('views', ['./views', 'views/list', 'views/auth', 'views/bee'] );
+app.set('views', ['./views', 'views/list', 'views/auth', 'views/bee']);
 app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT;
@@ -46,7 +46,7 @@ const server = app.listen(PORT, () => {
     debug(`Listee is listening to you on port ${chalk.green(PORT)}`);
 });
 
-function stop(){
+function stop() {
     server.close();
 }
 
--- services/OEDService.js
+++ services/OEDService.js
@@ -1,8 +1,30 @@
 const debug = require('debug')('app:OEDService');
+const axios = require('axios');
 
 function getPronunciation() {
-// TODO return audio from OED
+    debug('get entry');
+    const endPoint = 'entries';
+    const sourceLan = 'en-gb';
+    const word = 'badger';
+    axios.get( 'entries/en-gb/badger', {
+        baseURL: process.env.OED_BASE_URL,
+        params: {},
+        headers: {
+            'app_id': process.env.OED_APP_ID,
+            'app_key': process.env.OED_APP_KEY
+        }
+    })
+        .then(function (response) {
+            debug(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+    // TODO return audio from OED
     return 'audio goes here';
 }
 
-module.exports = { getPronunciation };
+module.exports = {getPronunciation};
